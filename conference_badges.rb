2925bad9fb9abde288b669aa7de98982e4b054c7
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 12:49:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,12 +12,12 @@ end
 return badgearray 
 end 
 
-def assign_rooms(attendees)
-  array = Array.new 
-  room = 1 
-  speakers.each do |attendee|
-    array << "Hello, #{attendees}! You'll be assigned to room #{room}!"
-    room += 1 
-  end 
-  array
+def assign_rooms(speakers)
+  rooms = Array.new 
+  room_num = 1 
+  speakers.each do |speaker|
+    rooms << "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    room_num += 1 
+  end #each
+  rooms
 end 
