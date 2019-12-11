722dd36de84264fdfa86cc3545b71e16f5bf6f5c
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 12:42:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+ end 
+
+
+def batch_badge_creator(array)
+  badgearray = []
+  array.each do |name| 
+    badgearray << "Hello, my name is #{name}."
+end 
+return badgearray 
+end 
+
+def assign_rooms(attendees)
+array = Array.new 
+  counter = 1 
+  attendees.each_with_index do |attendee|
+    attendees << "Hello, #{attendee}! You'll be assigned to room #{counter}!"
+    counter += 1 
+end 
+array
+end
